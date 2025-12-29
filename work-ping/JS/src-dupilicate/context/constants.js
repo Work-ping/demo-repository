46ad6ada6a13197772b91bea252d4f5712bb6c17
nrokzzzz <23A91A0581@aec.edu.in>
2025-12-29 10:23:35 +0530

    remove duplicate [nrokzzzz

--- work-ping/JS/src-dupilicate/context/constants.js
+++
@@ -1,9 +0,0 @@
-export const currency = '$';
-export const currentYear = new Date().getFullYear();
-export const developedByLink = 'https://techzaa.getappui.com/';
-export const developedBy = 'Techzaa';
-export const contactUs = 'techzaastudio@gmail.com';
-export const buyLink = '';
-export const basePath = '';
-export const DEFAULT_PAGE_TITLE = 'Work-Ping';
-export const colorVariants = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark', 'purple', 'pink', 'orange', 'light', 'link'];
