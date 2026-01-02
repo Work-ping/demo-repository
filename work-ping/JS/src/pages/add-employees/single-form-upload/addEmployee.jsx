bf66d915478fc24eac2ebda0f75c5e0463a6bb95
VinayakaGopalaKrishnA123 <chavalijsrvinayak09@gmail.com>
2026-01-02 14:02:07 +0530

    verifiedIds(JSR)

---
+++ work-ping/JS/src/pages/add-employees/single-form-upload/addEmployee.jsx
@@ -0,0 +1,16 @@
+// import EmployeeDetailsForm from "./EmployeeDetailsForm";
+// import FaceEmbeddings from "./faceEmbeddings";
+// import VerifiedIds from "./verifiedIds";
+// import { Button, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Row } from 'react-bootstrap';
+// const AddEmployee=()=>{
+//     return (<>
+//     <Button variant={Primary} className="rounded-pill me-1" type="button" key={idx}>
+//             {toSentenceCase(next)}
+//           </Button>
+//           <Button type="button" variant={`outline-${sucess}`} className="rounded-pill" key={idx}>
+//             {toSentenceCase(submit)}
+//           </Button>
+//     </>
+// );
+// }
+// export default AddEmployee;
