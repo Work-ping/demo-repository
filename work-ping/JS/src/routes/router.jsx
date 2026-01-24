5d3545cd1b02aabd506ce72bb07ac01cf83ac7c5
shaikumar0 <23A91A0560@aec.edu.in>
2026-01-24 09:51:01 +0530

    [umar] : removed cookies of fakebackend

--- work-ping/JS/src/routes/router.jsx
+++ work-ping/JS/src/routes/router.jsx
@@ -1,19 +1,28 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { useNavigate, Route, Routes } from 'react-router-dom';
 import AuthLayout from '@/layouts/AuthLayout';
 import { useAuthContext } from '@/context/useAuthContext';
 import { appRoutes, authRoutes } from '@/routes/index';
 import AdminLayout from '@/layouts/AdminLayout';
+import axiosClient from '@/helpers/httpClient';
+import { useEffect } from 'react';
 const AppRouter = props => {
-  const {
-    isAuthenticated
-  } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const verifyCookie = async () => {
+      try {
+        await axiosClient.get('/verify-cookie');
+      } catch(err) {
+        navigate('/auth/sign-in-customized');
+      }
+    }
+    verifyCookie();
+  }, [])
+
   return <Routes>
       {(authRoutes || []).map((route, idx) => <Route key={idx + route.name} path={route.path} element={<AuthLayout {...props}>{route.element}</AuthLayout>} />)}
 
-      {(appRoutes || []).map((route, idx) => <Route key={idx + route.name} path={route.path} element={isAuthenticated ? <AdminLayout {...props}>{route.element}</AdminLayout> : <Navigate to={{
-      pathname: '/auth/sign-in',
-      search: 'redirectTo=' + route.path
-    }} />} />)}
+      {(appRoutes || []).map((route, idx) => <Route key={idx + route.name} path={route.path} element={ <AdminLayout {...props}>{route.element}</AdminLayout> } />)}
     </Routes>;
 };
 export default AppRouter;
